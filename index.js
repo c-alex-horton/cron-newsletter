38c85f1c9e6b270d7770b7782903ae2c9bb504d5
Caleb Horton <c.alex.horton@gmail.com>
2022-12-17 13:23:23 -0600

    switched getRedditNews to separate files

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-// const rp = require('request-promise')
-const puppeteer = require('puppeteer')
-const url = 'https://www.reddit.com/r/news'
-const fs = require('fs')
-const cheerio = require('cheerio');
+const getRedditNews = require('./redditNewsScrapper')
 
-puppeteer
-    .launch()
-    .then((browser) => {
-        return browser.newPage();
-    })
-    .then((page) => {
-        return page.goto(url).then(function () {
-            return page.content();
-        });
-    })
-    .then((html) => {
-        const $ = cheerio.load(html, null, false)
-        const posts = $('.Post')
-        let headlines = []
+const main = async () => {
+    console.log('Pulling data...')
+    const data = await getRedditNews()
+    console.log(data);
+    process.exit()
+}
 
-        for (let i = 0; i < posts.length; i++) {
-            let postWrapper = $(posts[i]).find('h3')[0], postTitle = $(postWrapper).text()
-            let linkWrapper = $(posts[i]).find('a')[2], postLink = $(linkWrapper).attr('href')
-
-
-            let post = {
-                title: postTitle,
-                link: postLink
-            }
-
-            headlines.push(post)
-
-        }
-
-
-
-        console.log(headlines)
-        // fs.writeFile('output.txt', headlines, function (err) {
-        //     if (err) return console.log(err);
-        //     console.log('headlines > output.txt');
-        // });
-
-    }).then(() => process.exit())
-    .catch(function (err) {
-        console.log(err);
-        process.exit()
-    });
+main()
---
+++ redditNewsScrapper.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer')
+const url = 'https://www.reddit.com/r/news'
+const fs = require('fs')
+const cheerio = require('cheerio');
+
+
+const getRedditNews = async () => {
+    try {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage();
+
+        const html = await page.goto(url).then(function () {
+            return page.content();
+        });
+
+        const $ = cheerio.load(html, null, false)
+        const posts = $('.Post')
+        let headlines = []
+
+        for (let i = 0; i < posts.length; i++) {
+            let postWrapper = $(posts[i]).find('h3')[0], postTitle = $(postWrapper).text()
+            let linkWrapper = $(posts[i]).find('a')[2], postLink = $(linkWrapper).attr('href')
+
+            let post = {
+                title: postTitle,
+                link: postLink
+            }
+            headlines.push(post)
+        }
+
+        return headlines
+    } catch (error) {
+        console.log(error);
+        process.exit()
+    }
+
+
+}
+
+module.exports = getRedditNews
+
